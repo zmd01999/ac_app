0f36c27d6f9258eb10190ab33cbc697f781e92c6
詹梦迪 <386704305@qq.com>
2022-03-08 23:25:20 +0800

    http config

--- lib/http/http_util.dart
+++ lib/http/http_util.dart
@@ -1,31 +1,132 @@
 import 'package:dio/dio.dart';
-import 'dart:io';
-
-class HttpUtil {
-  final HOST = "127.0.0.1";
-// or new Dio with a BaseOptions instance.
-  var dio = Dio(BaseOptions(
-    baseUrl: 'http://httpbin.org/',
-    connectTimeout: 5000,
-    receiveTimeout: 100000,
-    // 5s
-    headers: {
-      HttpHeaders.userAgentHeader: 'dio',
-      'api': '1.0.0',
-    },
-    contentType: Headers.jsonContentType,
-    // Transform the response data to a String encoded with UTF8.
-    // The default value is [ResponseType.JSON].
-    responseType: ResponseType.plain,
-  ));
 
 
-  void getHttp() async {
-    try {
-      var response = await dio.get('http://www.google.com');
-      print(response);
-    } catch (e) {
-      print(e);
+class BaseUrl{
+  // 配置默认请求地址
+  static const String url = 'http://127.0.0.1';
+}
+
+class HttpUtil{
+  static void get(
+      String url,
+      {
+        required Map<String, dynamic> data,
+        required Map<String, dynamic> headers,
+        required Function success,
+        required Function error
+      }
+      ) async {
+
+    // 数据拼接
+    if(data != null && data.isNotEmpty){
+      StringBuffer options= new StringBuffer('?');
+      data.forEach((key, value){
+        options.write('${key}=${value}&');
+      });
+      String optionsStr = options.toString();
+      optionsStr = optionsStr.substring(0, optionsStr.length - 1);
+      url += optionsStr;
     }
+
+    // 发送get请求
+    await _sendRequest(
+        url,
+        'get',
+        success,
+        headers: headers,
+        error: error, data: {}
+    );
+  }
+
+  static void post(
+      String url,
+      {
+        required Map<String, dynamic> data,
+        required Map<String, dynamic> headers,
+        required Function success,
+        required Function error
+      }
+      ) async {
+
+    // 发送post请求
+    _sendRequest(
+        url,
+        'post',
+        success,
+        data: data,
+        headers: headers,
+        error: error
+    );
+  }
+
+  // 请求处理
+  static Future _sendRequest(
+      String url,
+      String method,
+      Function success,
+      {
+        required Map<String, dynamic> data,
+        required Map<String, dynamic> headers,
+        required Function error
+      }
+      ) async {
+    int _code;
+    String _msg;
+    var _backData;
+
+    // 检测请求地址是否是完整地址
+    if(!url.startsWith('http')){
+      url = BaseUrl.url + url;
+    }
+
+    try{
+      Map<String, dynamic> dataMap = data == null ? new Map() : data;
+      Map<String, dynamic> headersMap = headers == null ? new Map() : headers;
+
+      // 配置dio请求信息
+      Response response;
+      Dio dio = new Dio();
+      dio.options.connectTimeout = 10000;        // 服务器链接超时，毫秒
+      dio.options.receiveTimeout = 3000;         // 响应流上前后两次接受到数据的间隔，毫秒
+      dio.options.headers.addAll(headersMap);    // 添加headers,如需设置统一的headers信息也可在此添加
+
+      if(method == 'get'){
+        response = await dio.get(url);
+      }else{
+        response = await dio.post(url,data: dataMap);
+      }
+
+      if(response.statusCode != 200){
+        _msg = '网络请求错误,状态码:' + response.statusCode.toString();
+        _handError(error, _msg);
+        return;
+      }
+
+      // 返回结果处理
+      Map<String, dynamic> resCallbackMap = response.data;
+      _code = resCallbackMap['code'];
+      _msg = resCallbackMap['msg'];
+      _backData = resCallbackMap['data'];
+
+      if(success != null){
+        if(_code == 0){
+          success(_backData);
+        }else{
+          String errorMsg = _code.toString()+':'+_msg;
+          _handError(error, errorMsg);
+        }
+      }
+
+    }catch(exception){
+      _handError(error, '数据请求错误：'+exception.toString());
+    }
+  }
+
+  // 返回错误信息
+  static Future _handError(Function errorCallback,String errorMsg){
+    if(errorCallback == null){
+      return errorCallback(null);;
+    }
+     return errorCallback(errorMsg);
   }
 }
