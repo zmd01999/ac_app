67edc1092a4ffa6540bc84e896b81984bb4c51a9
詹梦迪 <386704305@qq.com>
2022-04-04 23:12:04 +0800

    feat: 设备中间页主题

---
+++ lib/constant/color_constants.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF2697FF);
+const secondaryColor =  Color.fromRGBO(182, 161, 236, 0.3);
+const bgColor = Color(0xFF212132);
+// const bgColor = Color(0xFF212121);
+const darkgreenColor = Color(0xFF2c614f);
+const greenColor = Color(0xFF6bab58);
+
+const defaultPadding = 16.0;
+const double defaultBorderRadius = 15;
+
+class ColorConstants {
+  static Color blue = Color(0xFF0D46BB);
+}
+
+class Palette {
+  static const Color background = Color(0xFFEDEEF2);
+  static const Color wrapperBg = Color(0xFF212121);
+}
